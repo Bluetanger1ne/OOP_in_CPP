acd54453c0fc890ca47578cbca79830019047ee2
Eli <eliel.levine@metropolia.fi>
2026-09-03 11:37:30 +0300

    Ex 5 works.. But jesus it was hard!

--- HW_2/Ex3.cpp
+++ HW_2/Ex3.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 
 std::vector<std::string> find_all_elements(const std::string &xml, std::string
-tag_name);
+                                           tag_name);
 
 
 int main() {
@@ -16,7 +16,7 @@ int main() {
     std::cin >> filename;
 
     std::ifstream file(filename);
-    if(!file.is_open()) {
+    if (!file.is_open()) {
         std::cerr << "Error. Unable to open file" << std::endl;
         return 1;
     }
@@ -27,38 +27,43 @@ int main() {
         if (tag == "stop") {
             return 0;
         }
-        while (std::getline(file, line)) { // Add all xml lines to a string
+        while (std::getline(file, line)) {
+            // Add all xml lines to a string
             contents.append(line);
             line.erase();
         }
         std::vector<std::string> elements = find_all_elements(contents, tag); // Find all elements
         std::cout << tag << ": ";
 
-        for (auto &item: elements) // Print them out
-        {
-            std::cout << item << " ";
+        if (elements.empty()) {
+            std::cout << "Not found" << " ";
+        } else {
+            for (auto &item: elements) // Print them out
+            {
+                std::cout << item << " ";
+            }
         }
         std::cout << " " << std::endl;
     }
 }
 
-std::vector<std::string> find_all_elements(const std::string &xml, std::string tag_name)
-{
+std::vector<std::string> find_all_elements(const std::string &xml, std::string tag_name) {
     std::vector<std::string> elements;
 
     std::string start_tag = "<" + tag_name + ">";
     std::string end_tag = "</" + tag_name + ">";
 
     std::size_t start = xml.find(start_tag); // Find the position of the first tag
+    std::size_t ending = xml.find(end_tag);
 
-    while (start != std::string::npos) // While there is stuff to read from the file
+    while (start != std::string::npos && ending != std::string::npos) // While there is stuff to read from the file
     {
-        start += start_tag.length();  // move past the start tag
-        std::size_t end = xml.find(end_tag, start); // Find the end tag
-        elements.push_back(xml.substr(start, end - start)); // add the string/text between the tags and add it to elements
-        start = xml.find(start_tag, end); // Move the start to the end of the end tag and continue.
+        start += start_tag.length(); // move past the start tag
+        elements.push_back(xml.substr(start, ending - start)); // add the string/text between the tags and add it to elements
+        start = xml.find(start_tag, ending); // Move the start to the end of the end tag and continue.
+        ending = xml.find(end_tag, ending);
+
     }
 
     return elements;
 }
-
---
+++ HW_3/Ex5.cpp
@@ -0,0 +1,195 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <compare>
+
+
+class Item {
+public:
+    Item() = default; //Default constructor
+    Item(std::string name, int weight, double price, int discount) { // Constructor
+        this->name = name;
+        this->weight = weight;
+        this->price = price;
+        this->discount = discount;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Item &item); // Output operator
+    friend std::istream &operator>>(std::istream &in, Item &item); // Output operator
+
+    int getDiscount() const {
+        return discount;
+    }
+    void setDiscount(int d) {
+        if (d >= 0 && d <= 100) {
+            discount = d;
+        }
+    }
+    double getPrice() const {
+        return price * (100 - getDiscount()) / 100.0;
+    }
+    void setPrice(double p) {
+        price = p;
+    }
+
+    auto operator<=>(const Item &other) const {
+        return (getPrice() / (weight / 1000.0)) <=> (other.getPrice() / (other.weight / 1000.0));
+    }
+
+    bool operator==(const Item &other) const {
+        double pricePerKg = getPrice() / (weight / 1000.0);
+        double otherPricePerKg = other.getPrice() / (other.weight / 1000.0);
+
+        return pricePerKg == otherPricePerKg;
+    }
+
+private:
+    std::string name;
+    int weight = 0;
+    double price = 0.0;
+    int discount = 0;
+};
+
+
+std::ostream &operator<<(std::ostream &out, const Item &item) {
+    out << "{ \"Name\":\"" << item.name
+            << "\", \"Weight\":" << item.weight
+            << ", \"Price\":" << item.price
+            << ", \"Discount\": " << item.discount
+            << " }";
+
+    return out;
+}
+
+std::istream &operator>>(std::istream &in, Item &item) {
+    std::string line;
+    std::getline(in >> std::ws, line); // get line and ignore white space.
+
+    std::size_t nameStart = line.find("\"Name\":\""); // find name location
+    std::size_t nameEnd = line.find("\",", nameStart);
+
+    std::size_t weightStart = line.find("\"Weight\":", nameEnd); // find weight location
+    std::size_t weightEnd = line.find(",", weightStart);
+
+    std::size_t priceStart = line.find("\"Price\":", weightEnd); //find price location
+    std::size_t priceEnd = line.find(",", priceStart);
+
+    std::size_t discountStart = line.find("\"Discount\":", priceEnd); // find discount location
+    std::size_t discountEnd = line.find("}", discountStart);
+
+    if (nameStart == std::string::npos || // Check that data is formatted correctly
+        nameEnd == std::string::npos ||
+        weightStart == std::string::npos ||
+        weightEnd == std::string::npos ||
+        priceStart == std::string::npos ||
+        priceEnd == std::string::npos ||
+        discountStart == std::string::npos ||
+        discountEnd == std::string::npos) {
+
+        in.setstate(std::ios::failbit);
+        return in;
+        }
+
+    std::string name = line.substr(nameStart + 8,nameEnd - (nameStart + 8));
+
+    try {
+        int weight = std::stoi(line.substr(weightStart + 9,weightEnd - (weightStart + 9)));
+        double price = std::stod(line.substr(priceStart + 8,priceEnd - (priceStart + 8)));
+        int discount = std::stoi(line.substr(discountStart + 11,discountEnd - (discountStart + 11)));
+        item = Item(name, weight, price, discount);
+    }
+    catch (...) {
+        in.setstate(std::ios::failbit);
+    }
+    return in;
+}
+
+
+int main() {
+    std::vector<Item> items;
+    std::string name;
+    bool asking = true;
+    int weight;
+    double price;
+    int discount;
+    while (asking) {
+        std::cout << "please enter the name of the item, \"json\" for JSON formatted data, or \"stop\" to stop: " << std::endl;
+        std::cin >> name;
+        if (!std::cin) {
+            std::cout << "Input failed!" << std::endl;
+            break;
+        }
+        if (name == "stop") {
+            asking = false;
+
+        } else if (name == "json") {
+            std::cout << "Please enter the JSON string: \n" << std::endl;
+            Item item;
+            std::cin >> item;
+            if (!std::cin) {
+                std::cout << "Input failed!" << std::endl;
+                std::cin.clear();
+            } else {
+                items.push_back(item);
+            }
+        } else {
+            std::cout << "please enter the weight of the item: " << std::endl;
+            std::cin >> weight;
+
+            std::cout << "please enter the price of the item: " << std::endl;
+            std::cin >> price;
+
+            std::cout << "please enter discount of the item: " << std::endl;
+            std::cin >> discount;
+
+            Item item(name, weight, price, discount);
+            items.push_back(item);
+        }
+    }
+    std::sort(items.begin(), items.end());
+    for (int i = 0; i < items.size(); i++) {
+        std::cout << i+1 << ": " << items[i] << std::endl;
+    }
+    double total = 0;
+
+    for (auto &item: items) {
+        double p = item.getPrice();
+        total += p;
+    }
+    std::cout << "\nNet worth: " << total << "\n" <<std::endl;
+
+    while (true) {
+        std::string choice;
+
+        std::cout << "Choose an item by index or enter \"stop\" to stop: ";
+        std::cin >> choice;
+        if (choice == "stop") {
+            return 0;
+        }
+        int index = std::stoi(choice);
+        if (index < 1 || index >= items.size()) {
+            std::cout << "Enter a valid index please..!" << std:: endl;
+        } else {
+            int disc;
+
+            std::cout << "Enter discount percentage: ";
+            std::cin >> disc;
+
+            items[index-1].setDiscount(disc);
+
+            std::sort(items.begin(), items.end());
+        }
+        for (int i = 0; i < items.size(); i++) {
+            std::cout << i+1 << ": " << items[i] << std::endl;
+        }
+        double total2 = 0;
+
+        for (const auto &item : items) {
+            total += item.getPrice();
+        }
+
+        std::cout << "\nNet worth: " << total2 << "\n" << std::endl;
+    }
+
+}
